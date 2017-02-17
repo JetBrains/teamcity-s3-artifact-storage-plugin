b00ab75050b62404e563c384bcf4d2872b0efd77
Victory Petrenko <victory.bedrosova@jetbrains.com>
2017-02-17 13:33:12 +0100

    store secret access key as a secure property

--- s3-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Constants.java
+++ s3-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Constants.java
@@ -8,7 +8,7 @@ public class S3Constants {
   public static final String S3_ARTIFACTS_LIST = "s3_artifacts_list.txt";
   public static final String S3_ARTIFACTS_LIST_PATH = ".teamcity/s3";
   public static final String S3_KEY_ID = "storage.s3.keyId";
-  public static final String S3_SECRET_KEY = "storage.s3.secretkey";
+  public static final String S3_SECRET_KEY = "secure:storage.s3.secretkey";
   public static final String S3_BUCKET_NAME = "storage.s3.bucket.name";
   public static final String S3_PATH_PREFIX = "storage.s3.path.prefix";
   public static final String S3_STORAGE_TYPE = "S3_storage";
