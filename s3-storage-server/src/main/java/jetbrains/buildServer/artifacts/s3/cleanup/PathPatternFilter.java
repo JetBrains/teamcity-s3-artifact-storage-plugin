8c2565f196fee587fa9ccf327f813d2eefa83019
Victory Petrenko <victory.bedrosova@jetbrains.com>
2017-03-15 12:41:15 +0100

    cleanup with respect to artifact clean-up patterns

---
+++ s3-storage-server/src/main/java/jetbrains/buildServer/artifacts/s3/cleanup/PathPatternFilter.java
@@ -0,0 +1,118 @@
+package jetbrains.buildServer.artifacts.s3.cleanup;
+
+import jetbrains.buildServer.util.CollectionsUtil;
+import jetbrains.buildServer.util.IncludeExcludeRules;
+import jetbrains.buildServer.util.StringUtil;
+import jetbrains.buildServer.util.pathMatcher.AntPatternTreeMatcher;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.*;
+
+/**
+ * @author vbedrosova
+ */
+public class PathPatternFilter {
+
+  public static final String DEFAULT_INCLUDE_RULE = "**/*";
+  public static final Collection<String> INCLUDE_ALL_PATTERNS = Arrays.asList(DEFAULT_INCLUDE_RULE, "+:**/*", "", "+.");
+
+  @NotNull private final List<String> myIncludePatterns = new ArrayList<>();
+  @NotNull private final List<String> myExcludePatterns = new ArrayList<>();
+
+  public PathPatternFilter(@NotNull String patterns) {
+    if (StringUtil.isEmptyOrSpaces(patterns)) {
+      patterns = "+:" + DEFAULT_INCLUDE_RULE;
+    }
+
+    final IncludeExcludeRules rules = new IncludeExcludeRules(patterns);
+    for (IncludeExcludeRules.Rule r : rules.getRules()) {
+      if (r.isInclude()) {
+        myIncludePatterns.add(r.getRule());
+      } else {
+        myExcludePatterns.add(r.getRule());
+      }
+    }
+  }
+
+  @NotNull
+  public List<String> filterPaths(@NotNull List<String> paths) {
+    if (isIncludeAll()) return paths;
+
+    final Collection<PathNode> files = AntPatternTreeMatcher.scan(createPathTree(paths), myIncludePatterns, myExcludePatterns, AntPatternTreeMatcher.ScanOption.LEAFS_ONLY);
+    return CollectionsUtil.convertCollection(files, PathNode::getPath);
+  }
+
+  private boolean isIncludeAll() {
+    return myExcludePatterns.isEmpty() && myIncludePatterns.size() == 1 && INCLUDE_ALL_PATTERNS.contains(myIncludePatterns.get(0));
+  }
+
+  @NotNull
+  /*package local*/ PathNode createPathTree(@NotNull List<String> paths) {
+    final List<String> pathsList = new ArrayList<>(paths);
+    Collections.sort(pathsList);
+
+    final PathNode root = new PathNode("");
+
+    for (String path : pathsList) {
+      PathNode parent = root;
+      for (String part : path.split("/")) {
+        final String parentPath = parent.getPath();
+        parent = parent.child(parentPath.isEmpty() ? part : parentPath + "/" + part);
+      }
+    }
+
+    return root;
+  }
+
+  /*package local*/  static class PathNode implements jetbrains.buildServer.util.pathMatcher.PathNode<PathNode> {
+    @NotNull private final String myPath;
+    @Nullable private Set<PathNode> myChildren;
+
+    public PathNode(@NotNull String path) {
+      if (path.endsWith("/")) path = path.substring(0, path.length() - 1);
+      if (path.startsWith("/")) path = path.substring(1);
+      myPath = path;
+      myChildren = null;
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+      final int slashIndex = myPath.lastIndexOf("/");
+      return slashIndex < 0 ? myPath : myPath.substring(slashIndex + 1);
+    }
+
+    @NotNull
+    public String getPath() {
+      return myPath;
+    }
+
+    @Nullable
+    @Override
+    public Iterable<PathNode> getChildren() {
+      return myChildren;
+    }
+
+    @NotNull
+    public PathNode child(@NotNull String path) {
+      if (myChildren == null) myChildren = new HashSet<>();
+
+      for (PathNode c : myChildren) {
+        if (path.equals(c.getPath())) return c;
+      }
+
+      final PathNode child = new PathNode(path);
+      myChildren.add(child);
+      return child;
+    }
+  }
+
+  /*package local*/ List<String> getIncludePatterns() {
+    return myIncludePatterns;
+  }
+
+  /*package local*/ List<String> getExcludePatterns() {
+    return myExcludePatterns;
+  }
+}
---
+++ s3-storage-server/src/test/java/jetbrains/buildServer/artifacts/s3/cleanup/PathPatternFilterTest.java
@@ -0,0 +1,179 @@
+package jetbrains.buildServer.artifacts.s3.cleanup;
+
+import jetbrains.buildServer.BaseTestCase;
+import jetbrains.buildServer.util.CollectionsUtil;
+import jetbrains.buildServer.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+/**
+ * @author vbedrosova
+ */
+@Test
+public class PathPatternFilterTest extends BaseTestCase {
+
+  @Test
+  public void path_tree() {
+    final PathPatternFilter.PathNode root = create("+:**/*").createPathTree(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals("", root.getName());
+    assertEquals("", root.getPath());
+
+    final List<PathPatternFilter.PathNode> rootChildren = toList(root.getChildren());
+    assertEquals(2, rootChildren.size());
+
+    final PathPatternFilter.PathNode some = getChild("some", rootChildren);
+    assertNotNull(some);
+    assertEquals("some", some.getName());
+    assertEquals("some", some.getPath());
+
+    final List<PathPatternFilter.PathNode> someChildren = toList(some.getChildren());
+    assertEquals(1, someChildren.size());
+
+    final PathPatternFilter.PathNode somePath = getChild("some/path", someChildren);
+    assertNotNull(somePath);
+    assertEquals("path", somePath.getName());
+    assertEquals("some/path", somePath.getPath());
+
+    final List<PathPatternFilter.PathNode> somePathChildren = toList(somePath.getChildren());
+    assertEquals(2, somePathChildren.size());
+
+    final PathPatternFilter.PathNode somePathAnother = getChild("some/path/another", somePathChildren);
+    assertNotNull(somePathAnother);
+    assertEquals("another", somePathAnother.getName());
+    assertEquals("some/path/another", somePathAnother.getPath());
+
+    final List<PathPatternFilter.PathNode> somePathAnotherChildren = toList(somePathAnother.getChildren());
+    assertEquals(1, somePathAnotherChildren.size());
+
+    final PathPatternFilter.PathNode somePathAnotherFile = getChild("some/path/another/file", somePathAnotherChildren);
+    assertNotNull(somePathAnotherFile);
+    assertEquals("file", somePathAnotherFile.getName());
+    assertEquals("some/path/another/file", somePathAnotherFile.getPath());
+    assertNull(somePathAnotherFile.getChildren());
+
+    final PathPatternFilter.PathNode somePathFile = getChild("some/path/file", somePathChildren);
+    assertNotNull(somePathFile);
+    assertEquals("file", somePathFile.getName());
+    assertEquals("some/path/file", somePathFile.getPath());
+    assertNull(somePathFile.getChildren());
+
+    final PathPatternFilter.PathNode another = getChild("another", rootChildren);
+    assertNotNull(another);
+    assertEquals("another", another.getName());
+    assertEquals("another", another.getPath());
+
+    final List<PathPatternFilter.PathNode> anotherChildren = toList(another.getChildren());
+    assertEquals(1, anotherChildren.size());
+
+    final PathPatternFilter.PathNode anotherPath = getChild("another/path", anotherChildren);
+    assertNotNull(anotherPath);
+    assertEquals("path", anotherPath.getName());
+    assertEquals("another/path", anotherPath.getPath());
+    assertNull(anotherPath.getChildren());
+  }
+
+  @Test
+  public void empty_pattern() {
+    final List<String> filtered = create(StringUtil.EMPTY).filterPaths(Arrays.asList("some/path", "another/path", "path"));
+    assertEquals(3, filtered.size());
+    assertContains(filtered, "some/path", "another/path", "path");
+  }
+
+  @Test
+  public void include_all() {
+    final List<String> filtered = create("**/*").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(3, filtered.size());
+    assertContains(filtered, "some/path/file", "some/path/another/file", "another/path");
+  }
+
+  @Test
+  public void include_file() {
+    final List<String> filtered = create("some/path/file").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(1, filtered.size());
+    assertContains(filtered, "some/path/file");
+  }
+
+  @Test
+  public void include_folder() {
+    assertEmpty(create("some/path").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path")));
+  }
+
+  @Test
+  public void include_folder_content() {
+    final List<String> filtered = create("some/path/*").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(1, filtered.size());
+    assertContains(filtered, "some/path/file");
+  }
+
+  @Test
+  public void include_folder_content_recursive1() {
+    final List<String> filtered = create("some/path/**").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(2, filtered.size());
+    assertContains(filtered, "some/path/file", "some/path/another/file");
+  }
+
+  @Test
+  public void include_folder_content_recursive2() {
+    final List<String> filtered = create("some/path/**/*").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(2, filtered.size());
+    assertContains(filtered, "some/path/file", "some/path/another/file");
+  }
+
+  @Test
+  public void exclude_file() {
+    final List<String> filtered = create("+:**/*\n-:some/path/file").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(2, filtered.size());
+    assertContains(filtered, "some/path/another/file", "another/path");
+  }
+
+  @Test
+  public void exclude_folder() {
+    final List<String> filtered = create("+:**/*\n-:some/path").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(3, filtered.size());
+    assertContains(filtered, "some/path/file", "some/path/another/file", "another/path");
+  }
+
+  @Test
+  public void exclude_folder_content() {
+    final List<String> filtered = create("+:**/*\n-:some/path/*").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(2, filtered.size());
+    assertContains(filtered,  "some/path/another/file", "another/path");
+  }
+
+  @Test
+  public void exclude_folder_content_recursive1() {
+    final List<String> filtered = create("+:**/*\n-:some/path/**").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(1, filtered.size());
+    assertContains(filtered,  "another/path");
+  }
+
+  @Test
+  public void exclude_folder_content_recursive2() {
+    final List<String> filtered = create("+:**/*\n-:some/path/**/*").filterPaths(Arrays.asList("some/path/file", "some/path/another/file", "another/path"));
+    assertEquals(1, filtered.size());
+    assertContains(filtered,  "another/path");
+  }
+
+  @NotNull
+  private PathPatternFilter create(@NotNull String pattern) {
+    return new PathPatternFilter(pattern);
+  }
+
+  @NotNull
+  private List<PathPatternFilter.PathNode> toList(@Nullable Iterable<PathPatternFilter.PathNode> nodes) {
+    if (nodes == null) return Collections.emptyList();
+
+    final ArrayList<PathPatternFilter.PathNode> result = new ArrayList<>();
+    final Iterator<PathPatternFilter.PathNode> it = nodes.iterator();
+    while (it.hasNext()) result.add(it.next());
+    return result;
+  }
+
+  @Nullable
+  private PathPatternFilter.PathNode getChild(@NotNull String path, @NotNull List<PathPatternFilter.PathNode> children) {
+    return CollectionsUtil.findFirst(children, data -> path.equals(data.getPath()));
+  }
+}
