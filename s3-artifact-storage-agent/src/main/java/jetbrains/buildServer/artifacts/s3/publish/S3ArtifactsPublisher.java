fd758ed433a363589934a61fa136cb4b5ec38ad0
ekoshkin <koshkinev@gmail.com>
2017-07-21 14:54:19 +0300

    upload artifacts to s3 via pre signed urls

--- s3-artifact-storage-agent/src/main/java/jetbrains/buildServer/artifacts/s3/publish/S3ArtifactsPublisher.java
+++ s3-artifact-storage-agent/src/main/java/jetbrains/buildServer/artifacts/s3/publish/S3ArtifactsPublisher.java
@@ -11,24 +11,27 @@ import jetbrains.buildServer.agent.*;
 import jetbrains.buildServer.agent.artifacts.AgentArtifactHelper;
 import jetbrains.buildServer.artifacts.ArtifactDataInstance;
 import jetbrains.buildServer.artifacts.s3.S3Util;
+import jetbrains.buildServer.http.HttpUtil;
 import jetbrains.buildServer.log.LogUtil;
 import jetbrains.buildServer.util.*;
 import jetbrains.buildServer.util.amazon.AWSClients;
 import jetbrains.buildServer.util.amazon.AWSCommonParams;
 import jetbrains.buildServer.util.amazon.AWSException;
 import jetbrains.buildServer.util.filters.Filter;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.FileRequestEntity;
+import org.apache.commons.httpclient.methods.PutMethod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.net.URL;
+import java.util.*;
 
 import static jetbrains.buildServer.artifacts.s3.S3Constants.*;
 import static jetbrains.buildServer.artifacts.s3.S3Util.getBucketName;
+import static jetbrains.buildServer.artifacts.s3.S3Util.usePreSignedUrls;
 
 public class S3ArtifactsPublisher implements ArtifactsPublisher {
 
@@ -36,15 +39,19 @@ public class S3ArtifactsPublisher implements ArtifactsPublisher {
   private static final String ERROR_PUBLISHING_ARTIFACTS_LIST = "Error publishing artifacts list";
 
   private final CurrentBuildTracker myTracker;
+  private final S3PreSignedUrlResolver myS3PreSignedUrlResolver;
   private final AgentArtifactHelper myHelper;
+
   private boolean isDestinationPrepared = false;
   private List<ArtifactDataInstance> myArtifacts = new ArrayList<ArtifactDataInstance>();
 
   public S3ArtifactsPublisher(@NotNull final AgentArtifactHelper helper,
                               @NotNull final EventDispatcher<AgentLifeCycleListener> dispatcher,
-                              @NotNull final CurrentBuildTracker tracker) {
+                              @NotNull final CurrentBuildTracker tracker,
+                              @NotNull final S3PreSignedUrlResolver s3PreSignedUrlResolver) {
     myHelper = helper;
     myTracker = tracker;
+    myS3PreSignedUrlResolver = s3PreSignedUrlResolver;
     dispatcher.addListener(new AgentLifeCycleAdapter() {
       @Override
       public void buildStarted(@NotNull AgentRunningBuild runningBuild) {
@@ -61,38 +68,24 @@ public class S3ArtifactsPublisher implements ArtifactsPublisher {
 
   @Override
   public int publishFiles(@NotNull final Map<File, String> map) throws ArtifactPublishingFailedException {
-    final List<Map.Entry<File, String>> filesToPublish = CollectionsUtil.filterCollection(map.entrySet(), new Filter<Map.Entry<File, String>>() {
+    Map<File, String> filteredMap = CollectionsUtil.filterMapByValues(map, new Filter<String>() {
       @Override
-      public boolean accept(@NotNull Map.Entry<File, String> entry) {
-        return !entry.getValue().startsWith(ArtifactsConstants.TEAMCITY_ARTIFACTS_DIR);
+      public boolean accept(@NotNull String s) {
+        return !s.startsWith(ArtifactsConstants.TEAMCITY_ARTIFACTS_DIR);
       }
     });
 
-    if (!filesToPublish.isEmpty()) {
+    if (!filteredMap.isEmpty()) {
       final AgentRunningBuild build = myTracker.getCurrentBuild();
-      try {
-        final Map<String, String> params = getPublisherParameters();
-        final String bucketName = getBucketName(params);
-        prepareDestination(bucketName, params, build);
-
-        final String pathPrefix = getPathPrefixProperty(build);
-        myArtifacts.addAll(publishArtifacts(bucketName, pathPrefix, params, filesToPublish));
-      } catch (ArtifactPublishingFailedException t) {
-        throw t;
-      } catch (Throwable t) {
-        final AWSException awsException = new AWSException(t);
-        final String details = awsException.getDetails();
-
-        if (StringUtil.isNotEmpty(details)) {
-          LOG.warn(details);
-          myTracker.getCurrentBuild().getBuildLogger().error(details);
-        }
-
-        throw new ArtifactPublishingFailedException(awsException.getMessage(), false, awsException);
+      final Map<String, String> params = getPublisherParameters();
+      if(usePreSignedUrls(params)){
+        myArtifacts.addAll(publishFilesWithPreSignedUrls(build, filteredMap));
+      } else {
+        myArtifacts.addAll(publishFilesWithS3Client(build, params, filteredMap));
       }
     }
 
-    return filesToPublish.size();
+    return filteredMap.size();
   }
 
   @Override
@@ -106,38 +99,92 @@ public class S3ArtifactsPublisher implements ArtifactsPublisher {
     return S3_STORAGE_TYPE;
   }
 
-  @NotNull
-  private List<ArtifactDataInstance> publishArtifacts(final String bucketName,
-                                                      final String pathPrefix,
-                                                      final Map<String, String> params,
-                                                      final List<Map.Entry<File, String>> entries) throws Throwable {
-    final List<ArtifactDataInstance> artifacts = new ArrayList<ArtifactDataInstance>();
-    jetbrains.buildServer.util.amazon.S3Util.withTransferManager(params, new jetbrains.buildServer.util.amazon.S3Util.WithTransferManager<Upload>() {
-      @NotNull
-      @Override
-      public Collection<Upload> run(@NotNull final TransferManager transferManager) throws Throwable {
-        return CollectionsUtil.convertAndFilterNulls(entries, new Converter<Upload, Map.Entry<File, String>>() {
-          @Override
-          public Upload createFrom(@NotNull Map.Entry<File, String> entry) {
-            final File file = entry.getKey();
-            final String path = entry.getValue();
-            final String artifactPath = preparePath(path, file);
+  private List<ArtifactDataInstance> publishFilesWithS3Client(AgentRunningBuild build, Map<String, String> params, final Map<File, String> filesToPublish) {
+    final String bucketName = getBucketName(params);
+    try {
+      prepareDestination(bucketName, params, build);
+      final String objectKeyPrefix = getS3ObjectKeyPrefixProperty(build);
+      final List<ArtifactDataInstance> artifacts = new ArrayList<ArtifactDataInstance>();
+      jetbrains.buildServer.util.amazon.S3Util.withTransferManager(params, new jetbrains.buildServer.util.amazon.S3Util.WithTransferManager<Upload>() {
+        @NotNull
+        @Override
+        public Collection<Upload> run(@NotNull final TransferManager transferManager) throws Throwable {
+          return CollectionsUtil.convertAndFilterNulls(filesToPublish.entrySet(), new Converter<Upload, Map.Entry<File, String>>() {
+            @Override
+            public Upload createFrom(@NotNull Map.Entry<File, String> entry) {
+              final File file = entry.getKey();
+              final String path = entry.getValue();
+              final String artifactPath = normalizeArtifactPath(path, file);
 
-            final String objectPath = pathPrefix + artifactPath;
+              final String objectKey = objectKeyPrefix + artifactPath;
 
-            artifacts.add(ArtifactDataInstance.create(artifactPath, file.length()));
-            return transferManager.upload(new PutObjectRequest(
-              bucketName,
-              objectPath,
-              file).withCannedAcl(CannedAccessControlList.Private));
-          }
-        });
+              artifacts.add(ArtifactDataInstance.create(artifactPath, file.length()));
+
+              return transferManager.upload(new PutObjectRequest(
+                bucketName,
+                objectKey,
+                file).withCannedAcl(CannedAccessControlList.Private));
+            }
+          });
+        }
+      });
+      return artifacts;
+    } catch (ArtifactPublishingFailedException t) {
+      throw t;
+    } catch (Throwable t) {
+      final AWSException awsException = new AWSException(t);
+      final String details = awsException.getDetails();
+
+      if (StringUtil.isNotEmpty(details)) {
+        LOG.warn(details);
+        myTracker.getCurrentBuild().getBuildLogger().error(details);
       }
-    });
-    return artifacts;
+
+      throw new ArtifactPublishingFailedException(awsException.getMessage(), false, awsException);
+    }
   }
 
-  private String preparePath(final String path, final File file) {
+  private List<ArtifactDataInstance> publishFilesWithPreSignedUrls(final AgentRunningBuild build, Map<File, String> filesToPublish) {
+    final Map<File, String> fileToNormalizedArtifactPathMap = new HashMap<File, String>();
+    final Map<File, String> fileToS3ObjectKeyMap = new HashMap<File, String>();
+    final String s3ObjectKeyPrefix = getS3ObjectKeyPrefixProperty(build);
+
+    for (Map.Entry<File, String> entry : filesToPublish.entrySet()){
+      String normalizeArtifactPath = normalizeArtifactPath(entry.getValue(), entry.getKey());
+      fileToNormalizedArtifactPathMap.put(entry.getKey(), normalizeArtifactPath);
+      fileToS3ObjectKeyMap.put(entry.getKey(), s3ObjectKeyPrefix + normalizeArtifactPath);
+    }
+
+    final Map<String, URL> preSignedUploadUrls = myS3PreSignedUrlResolver.resolveUploadUrls(fileToS3ObjectKeyMap.values());
+    final int connectionTimeout = build.getAgentConfiguration().getServerConnectionTimeout();
+
+    return CollectionsUtil.convertAndFilterNulls(filesToPublish.keySet(), new Converter<ArtifactDataInstance, File>() {
+      @Override
+      public ArtifactDataInstance createFrom(@NotNull File file) {
+        String artifactPath = fileToNormalizedArtifactPathMap.get(file);
+        URL uploadUrl = preSignedUploadUrls.get(fileToS3ObjectKeyMap.get(file));
+        if(uploadUrl == null){
+          throw new ArtifactPublishingFailedException("Failed to publish artifact " + artifactPath + ". Can't get presigned upload url.", false, null);
+        }
+        HttpClient httpClient = HttpUtil.createHttpClient(connectionTimeout, uploadUrl, null);
+        try {
+          PutMethod putMethod = new PutMethod();
+          putMethod.setRequestEntity(new FileRequestEntity(file, "application/octet-stream"));
+          int responseCode = httpClient.executeMethod(putMethod);
+          if(responseCode == 200){
+            LOG.debug(String.format("Succesfully upload artifact %s to %s", artifactPath, uploadUrl));
+          } else{
+            throw new ArtifactPublishingFailedException(String.format("Failed upload artifact %s to %s. Response code received: %d.", artifactPath, uploadUrl, responseCode), false, null);
+          }
+        } catch (IOException ex){
+          throw new ArtifactPublishingFailedException(ex.getMessage(), false, ex);
+        }
+        return ArtifactDataInstance.create(artifactPath, file.length());
+      }
+    });
+  }
+
+  private String normalizeArtifactPath(final String path, final File file) {
     if (StringUtil.isEmpty(path)) {
       return file.getName();
     } else {
@@ -174,7 +221,7 @@ public class S3ArtifactsPublisher implements ArtifactsPublisher {
 
   private void publishArtifactsList(AgentRunningBuild build) {
     if (!myArtifacts.isEmpty()) {
-      final String pathPrefix = getPathPrefixProperty(build);
+      final String pathPrefix = getS3ObjectKeyPrefixProperty(build);
       try {
         myHelper.publishArtifactList(myArtifacts, CollectionsUtil.asMap(S3_PATH_PREFIX_ATTR, pathPrefix));
       } catch (IOException e) {
@@ -190,7 +237,7 @@ public class S3ArtifactsPublisher implements ArtifactsPublisher {
   }
 
   @NotNull
-  private String getPathPrefixProperty(@NotNull AgentRunningBuild build) throws ArtifactPublishingFailedException {
+  private String getS3ObjectKeyPrefixProperty(@NotNull AgentRunningBuild build) throws ArtifactPublishingFailedException {
     final String prefix = build.getSharedBuildParameters().getSystemProperties().get(S3_PATH_PREFIX_SYSTEM_PROPERTY);
     if (StringUtil.isEmptyOrSpaces(prefix)) {
       throw new ArtifactPublishingFailedException("No " + S3_PATH_PREFIX_SYSTEM_PROPERTY + " build system property found", false, null);
