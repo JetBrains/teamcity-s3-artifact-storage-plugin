efa5afd819296fdf76273b8d515a4bc10c97f1ad
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2020-07-03 15:53:33 +0200

    TW-66631 Lots of "Address already in use: connect" errors from S3 plugin in teamcity-server.log
     - Temporarily disable shutting down client

--- s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Util.java
+++ s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3Util.java
@@ -174,7 +174,8 @@ public class S3Util {
   }
 
   public static <T, E extends Throwable> T withS3ClientShuttingDownImmediately(@NotNull final Map<String, String> params, @NotNull final WithS3<T, E> withClient) throws E {
-    return withS3Client(params, withClient, true);
+    //TODO: Dmitrii Bogdanov Immediate shutting down is disabled until the correct handling of all the places where request might be prematurely closed due to shutted down client
+    return withS3Client(params, withClient, false);
   }
 
   public static <T, E extends Throwable> T withS3Client(@NotNull final Map<String, String> params, @NotNull final WithS3<T, E> withClient) throws E {
