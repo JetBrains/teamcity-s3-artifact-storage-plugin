2a05c28a6d699d01174e1584d28242a0470a01fd
dmitrii.kirkhmeier <dmitrii.kirkhmeier@jetbrains.com>
2022-04-21 16:39:27 +0200

    TW-75512 Fixed S3 generator
    (cherry picked from commit c9f89324172f18dbf9482f080f517ed9eb208286)

--- teamcity-s3-sdk/src/main/java/jetbrains/buildServer/filestorage/S3PresignedUrlProviderImpl.java
+++ teamcity-s3-sdk/src/main/java/jetbrains/buildServer/filestorage/S3PresignedUrlProviderImpl.java
@@ -25,7 +25,6 @@ import com.intellij.openapi.util.text.StringUtil;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Function;
-import jetbrains.buildServer.artifacts.s3.S3Constants;
 import jetbrains.buildServer.artifacts.s3.S3Util;
 import jetbrains.buildServer.serverSide.TeamCityProperties;
 import jetbrains.buildServer.util.TimeService;
@@ -100,7 +99,7 @@ public class S3PresignedUrlProviderImpl implements S3PresignedUrlProvider {
         }
       }
       //This header ensures that bucket owner always has access to uploaded objects
-      if ((httpMethod == HttpMethod.PUT || httpMethod == HttpMethod.POST) && settings.toRawSettings().containsKey(S3Constants.S3_ACL) && nPart == null) {
+      if ((httpMethod == HttpMethod.PUT || httpMethod == HttpMethod.POST) && nPart == null) {
         request.putCustomRequestHeader("x-amz-acl", settings.getAcl().toString());
       }
       return callS3(client -> client.generatePresignedUrl(request).toString(), settings);
