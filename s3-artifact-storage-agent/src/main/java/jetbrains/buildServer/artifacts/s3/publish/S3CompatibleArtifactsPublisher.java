f93ec081314ff2d3559c31e1a2ccd860941e4d1a
Andrei Efimov <andrei.efimov@jetbrains.com>
2023-05-15 13:40:10 +0200

    fix: TW-81280 S3 Storage new UI: the artifacts are not published to S3 Compatible storages

---
+++ s3-artifact-storage-agent/src/main/java/jetbrains/buildServer/artifacts/s3/publish/S3CompatibleArtifactsPublisher.java
@@ -0,0 +1,33 @@
+package jetbrains.buildServer.artifacts.s3.publish;
+
+import jetbrains.buildServer.ExtensionHolder;
+import jetbrains.buildServer.agent.AgentLifeCycleListener;
+import jetbrains.buildServer.agent.BuildAgentConfiguration;
+import jetbrains.buildServer.agent.CurrentBuildTracker;
+import jetbrains.buildServer.agent.artifacts.AgentArtifactHelper;
+import jetbrains.buildServer.artifacts.s3.publish.presigned.upload.PresignedUrlsProviderClientFactory;
+import jetbrains.buildServer.util.EventDispatcher;
+import org.jetbrains.annotations.NotNull;
+
+import static jetbrains.buildServer.artifacts.s3.S3Constants.S3_COMPATIBLE_STORAGE_TYPE;
+
+/**
+ * Duplicate S3ArtifactsPublisher to use S3 Compatible storage.type
+ */
+public class S3CompatibleArtifactsPublisher extends S3ArtifactsPublisher {
+  public S3CompatibleArtifactsPublisher(@NotNull AgentArtifactHelper helper,
+                                        @NotNull EventDispatcher<AgentLifeCycleListener> dispatcher,
+                                        @NotNull CurrentBuildTracker tracker,
+                                        @NotNull BuildAgentConfiguration buildAgentConfiguration,
+                                        @NotNull PresignedUrlsProviderClientFactory presignedUrlsProviderClient,
+                                        @NotNull S3FileUploaderFactory uploaderFactory,
+                                        @NotNull ExtensionHolder extensionHolder) {
+    super(helper, dispatcher, tracker, buildAgentConfiguration, presignedUrlsProviderClient, uploaderFactory, extensionHolder);
+  }
+
+  @NotNull
+  @Override
+  public String getType() {
+    return S3_COMPATIBLE_STORAGE_TYPE;
+  }
+}
