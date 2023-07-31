3deacf754924643a5d0ec9bf65ef6aa2c85fe00e
Andrei Efimov <andrei.efimov@jetbrains.com>
2023-07-31 09:32:29 +0000

    fix: TW-81957: [S3 Artifact Storage] Uploads failing with org.apache.http.NoHttpResponseException
    Merge-request: TC-MR-6320
    Merged-by: Andrei Efimov <Andrei.Efimov@jetbrains.com>

--- teamcity-s3-sdk/src/main/java/jetbrains/buildServer/artifacts/s3/publish/S3FileUploaderFactoryImpl.java
+++ teamcity-s3-sdk/src/main/java/jetbrains/buildServer/artifacts/s3/publish/S3FileUploaderFactoryImpl.java
@@ -5,17 +5,19 @@ import jetbrains.buildServer.artifacts.s3.S3Configuration;
 import jetbrains.buildServer.artifacts.s3.publish.logger.S3UploadLogger;
 import jetbrains.buildServer.artifacts.s3.publish.presigned.upload.PresignedUrlsProviderClient;
 import jetbrains.buildServer.artifacts.s3.publish.presigned.upload.S3SignedUrlFileUploader;
+import jetbrains.buildServer.serverSide.TeamCityProperties;
 import org.jetbrains.annotations.NotNull;
 
-import static jetbrains.buildServer.artifacts.s3.S3Constants.S3_STORAGE_TYPE;
-import static jetbrains.buildServer.artifacts.s3.S3Constants.TEAMCITY_STORAGE_TYPE_KEY;
+import static jetbrains.buildServer.artifacts.s3.S3Constants.*;
 
 public class S3FileUploaderFactoryImpl implements S3FileUploaderFactory {
   @Override
   public S3FileUploader create(@NotNull final S3Configuration s3Configuration,
                                @NotNull final S3UploadLogger s3UploadLogger,
                                @NotNull final Supplier<PresignedUrlsProviderClient> presignedUrlsProviderClientSupplier) {
-    if (S3_STORAGE_TYPE.equals(s3Configuration.getSettingsMap().get(TEAMCITY_STORAGE_TYPE_KEY))) {
+
+    if (TeamCityProperties.getBoolean(S3_FORCE_PRESIGNED_URLS, true) &&
+        S3_STORAGE_TYPE.equals(s3Configuration.getSettingsMap().get(TEAMCITY_STORAGE_TYPE_KEY))) {
       return new S3SignedUrlFileUploader(s3Configuration, s3UploadLogger, presignedUrlsProviderClientSupplier);
     }
 
