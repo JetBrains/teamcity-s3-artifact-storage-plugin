43f2a9f8a754bab597705bc4c7579113fd968c54
nastasia.chubatova <nastasia.chubatova@jetbrains.com>
2025-04-08 17:47:21 +0200

    Collect js-related thirt-party libraries

--- s3-artifact-storage-ui/webpack.config.js
+++ s3-artifact-storage-ui/webpack.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
-
 const getWebpackConfig = require('@jetbrains/teamcity-api/getWebpackConfig');
+const LicenseChecker = require('@jetbrains/ring-ui-license-checker');
 
-module.exports = getWebpackConfig({
-  srcPath: path.join(__dirname, './src'),
-  outputPath: path.resolve(__dirname, '../s3-artifact-storage-server/src/main/resources/buildServerResources'),
-  entry: './src/index.tsx',
-  useTypeScript: true
+function createLicenseChecker(filename) {
+    return new LicenseChecker({
+        format: require('./third-party-licenses-json'),
+        filename,
+        exclude: [/@jetbrains/],
+        surviveLicenseErrors: true,
+    });
+}
+
+const createConfig = getWebpackConfig({
+    srcPath: path.join(__dirname, './src'),
+    outputPath: path.resolve(__dirname, '../s3-artifact-storage-server/src/main/resources/buildServerResources'),
+    entry: './src/index.tsx',
+    useTypeScript: true
 });
+
+const config = createConfig();
+config.plugins.push(createLicenseChecker('../../../../../s3-artifact-storage-ui/js-related-libraries.json'))
+
+module.exports = config;
