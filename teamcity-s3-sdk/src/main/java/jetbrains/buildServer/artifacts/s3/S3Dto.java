ed5fe971a92d49d000839d4fac4bb7ada9e4e2ae
Vladimir I <vladimir.ilmov@jetbrains.com>
2026-01-20 14:00:50 +0100

    teamcity-s3-sdk moved into Core

--- teamcity-s3-sdk/src/main/java/jetbrains/buildServer/artifacts/s3/S3Dto.java
+++
@@ -1,4 +0,0 @@
-package jetbrains.buildServer.artifacts.s3;
-
-public interface S3Dto {
-}
--- teamcity-s3-sdk/src/main/java/jetbrains/buildServer/artifacts/s3/publish/presigned/upload/PublishingInterruptedException.java
+++
@@ -1,9 +0,0 @@
-package jetbrains.buildServer.artifacts.s3.publish.presigned.upload;
-
-import org.jetbrains.annotations.NotNull;
-
-public class PublishingInterruptedException extends RuntimeException {
-  public PublishingInterruptedException(@NotNull final String reason) {
-    super(reason);
-  }
-}
