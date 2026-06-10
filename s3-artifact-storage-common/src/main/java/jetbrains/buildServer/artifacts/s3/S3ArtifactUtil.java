601ac3311332104bc1ab31a43a077db96901ebb7
Kirill (KIR) Maximov <kir@maxkir.com>
2026-06-10 17:26:41 +0200

    TW-101373 Backslash ("/") in S3 path prefix leads to signature mismatch

--- s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3ArtifactUtil.java
+++ s3-artifact-storage-common/src/main/java/jetbrains/buildServer/artifacts/s3/S3ArtifactUtil.java
@@ -2,27 +2,28 @@ package jetbrains.buildServer.artifacts.s3;
 
 import java.util.ArrayList;
 import java.util.List;
-import jetbrains.buildServer.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import static jetbrains.buildServer.util.StringUtil.*;
 
 public class S3ArtifactUtil {
 
   @NotNull
   public static String getPathPrefix(@Nullable final String customPrefix, @NotNull final String projectId, @NotNull final String buildTypeId, final long buildId) {
     final List<String> pathSegments = new ArrayList<>();
-    if (!StringUtil.isEmptyOrSpaces(customPrefix)) {
-      pathSegments.add(customPrefix);
+    if (!isEmptyOrSpaces(customPrefix)) {
+      pathSegments.add(removeLeadingAndTailingSlash(customPrefix));
     }
     pathSegments.add(projectId);
     pathSegments.add(buildTypeId);
     pathSegments.add(Long.toString(buildId));
-    return StringUtil.join("/", pathSegments) + "/";
+    return join("/", pathSegments) + "/";
   }
 
   public static boolean matchBuildId(String prefix, String key, long buildId) {
     int idx;
-    if (StringUtil.isEmpty(prefix)) {
+    if (isEmpty(prefix)) {
       idx = 0;
     } else if (key.startsWith(prefix + "/")) {
       idx = prefix.length() + 1;
--- s3-artifact-storage-common/src/test/java/jetbrains/buildServer/artifacts/s3/S3UtilTest.java
+++ s3-artifact-storage-common/src/test/java/jetbrains/buildServer/artifacts/s3/S3UtilTest.java
@@ -3,9 +3,12 @@
 package jetbrains.buildServer.artifacts.s3;
 
 import java.io.File;
+import jetbrains.buildServer.util.TestFor;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 @Test
 public class S3UtilTest {
@@ -27,5 +30,15 @@ public class S3UtilTest {
   @Test(dataProvider = "getContentTypeData")
   public void getContentTypeTest(String fileName, String expectedType) {
     Assert.assertEquals(S3Util.getContentType(new File("S3UtilsTest", fileName)), expectedType);
+  }
+
+  @Test
+  @TestFor(issues = "TW-101373")
+  public void test_getPathPrefix() {
+    then(S3ArtifactUtil.getPathPrefix(null, "x", "y", 1)).isEqualTo("x/y/1/");
+    then(S3ArtifactUtil.getPathPrefix(" ", "x", "y", 1)).isEqualTo("x/y/1/");
+
+    then(S3ArtifactUtil.getPathPrefix("a/", "x", "y", 1)).isEqualTo("a/x/y/1/");
+    then(S3ArtifactUtil.getPathPrefix("/a/", "x", "y", 1)).isEqualTo("a/x/y/1/");
   }
 }
